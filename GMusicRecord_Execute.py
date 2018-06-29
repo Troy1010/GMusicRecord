c9c8ffcb9cb1495f4dc12ffad6ebc901bd42003f
Unknown <drmcmurderface2@mail.com>
2018-06-28 20:49:57 -0500

    Pause on more exceptions

--- GMusicRecord_Execute.py
+++ GMusicRecord_Execute.py
@@ -1,36 +1,36 @@
-##region Settings
-bWriteLog = True
-##endregion
-##region LogInit
-import logging, os
-sLogFile = os.path.join(__file__,'..','GMR_ExecuteLog.log')
-GMR_ExecuteLog = logging.getLogger(__name__)
-GMR_ExecuteLog.setLevel(logging.DEBUG)
 try:
-    os.remove(sLogFile)
-except (PermissionError,FileNotFoundError):
-    pass
-if bWriteLog:
-    GMR_ExecuteLog.addHandler(logging.FileHandler(sLogFile))
-##endregion
+    ##region Settings
+    bWriteLog = True
+    ##endregion
+    ##region LogInit
+    import logging, os
+    sLogFile = os.path.join(__file__,'..','GMR_ExecuteLog.log')
+    GMR_ExecuteLog = logging.getLogger(__name__)
+    GMR_ExecuteLog.setLevel(logging.DEBUG)
+    try:
+        os.remove(sLogFile)
+    except (PermissionError,FileNotFoundError):
+        pass
+    if bWriteLog:
+        GMR_ExecuteLog.addHandler(logging.FileHandler(sLogFile))
+    ##endregion
 
-GMR_ExecuteLog.debug("Installing GmusicRecord")
-import subprocess
-subprocess.run(['python', 'setup.py', 'install'])
+    GMR_ExecuteLog.debug("Installing GmusicRecord")
+    import subprocess
+    subprocess.run(['python', 'setup.py', 'install'])
 
-##region Imports
-import GMusicRecord
-import TM_CommonPy as TM
-import traceback, sys
-##endregion
+    ##region Imports
+    import GMusicRecord
+    import TM_CommonPy as TM
+    import traceback, sys
+    ##endregion
 
-try:
+
     GMR_ExecuteLog.debug("Making a record")
     TM.MakeDir("Workspace",bCDInto=True)
     GMusicRecord.DownloadAndCommitRecord()
     os.chdir("..")
     TM.Delete("Workspace")
-    qwer
 except Exception as e:
     print("====================================================================")
     print("Traceback (most recent call last):")
