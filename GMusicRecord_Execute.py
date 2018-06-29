504958e45cb0cdecb7e84e2cd6641abbb66e6083
Unknown <drmcmurderface2@mail.com>
2018-06-28 20:19:44 -0500

    Pause on exception

--- GMusicRecord_Execute.py
+++ GMusicRecord_Execute.py
@@ -21,10 +21,20 @@ subprocess.run(['python', 'setup.py', 'install'])
 ##region Imports
 import GMusicRecord
 import TM_CommonPy as TM
+import traceback, sys
 ##endregion
 
-GMR_ExecuteLog.debug("Making a record")
-TM.MakeDir("Workspace",bCDInto=True)
-GMusicRecord.DownloadAndCommitRecord()
-os.chdir("..")
-TM.Delete("Workspace")
+try:
+    GMR_ExecuteLog.debug("Making a record")
+    TM.MakeDir("Workspace",bCDInto=True)
+    GMusicRecord.DownloadAndCommitRecord()
+    os.chdir("..")
+    TM.Delete("Workspace")
+    qwer
+except Exception as e:
+    print("====================================================================")
+    print("Traceback (most recent call last):")
+    traceback.print_tb(e.__traceback__)
+    print(e)
+    os.system('pause')
+    sys.exit(1)
